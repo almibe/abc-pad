5fdc397ec552394d352bc274df3e2bd629666564
Alex Berry <berry3@indiana.edu>
2016-08-30 21:31:18 -0400

    adding basic project structure

---
+++ src/main/kotlin/org/libraryweasel/music/Activator.kt
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.libraryweasel.music
+
+import org.apache.felix.dm.DependencyActivatorBase
+import org.apache.felix.dm.DependencyManager
+import org.libraryweasel.servo.LibraryWeaselComponentRegistrar
+import org.osgi.framework.BundleContext
+
+class Activator : DependencyActivatorBase() {
+    override fun init(context: BundleContext, manager: DependencyManager) {
+        val registrar = LibraryWeaselComponentRegistrar(manager)
+
+    }
+
+    override fun destroy(context: BundleContext, manager: DependencyManager) {
+
+    }
+}
