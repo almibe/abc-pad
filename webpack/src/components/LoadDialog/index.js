1d2aed740d2bdc72cf3dd960e0fbd03058a7e28b
Alex Berry <berry3@indiana.edu>
2018-01-21 17:24:43 -0500

    starting to move view code into components, #13

---
+++ webpack/src/components/LoadDialog/index.js
@@ -0,0 +1,27 @@
+import xs from 'xstream'
+import { html } from 'snabbdom-jsx'
+
+export function LoadButton(sources) {
+  const domSource = sources.DOM
+  const state$ = sources.state
+
+  const vdom$ = state$.map( state =>
+    <div className="modal">
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Load Document</p>
+          <button className="delete" aria-label="close"></button>
+        </header>
+        <section className="modal-card-body">
+          Content
+        </section>
+        <footer className="modal-card-foot">
+          <button className="button is-success">Load</button>
+          <button className="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
+  )
+  return { DOM: vdom$ }
+}
--- webpack/src/main.js
+++ webpack/src/main.js
@@ -4,73 +4,14 @@ import { makeDOMDriver } from '@cycle/dom'
 import { makeHTTPDriver } from '@cycle/http'
 import { html } from 'snabbdom-jsx'
 import ABCJS from '../node_modules/abcjs/bin/abcjs_editor_latest-min.js'
+import ABCEditor from 'components/ABCEditor'
+import Header from 'components/Header'
+import LoadDialog from 'component/LoadDialog'
 import './main.scss'
 
 function view(state$) {
   return state$.map( state =>
     <section className="section">
-      <div className="container">
-        <h1 className="title">ABC Editor</h1>
-      </div>
-
-      <div className="container">
-        <div className="columns">
-          <div className="column">
-            <div className="field is-horizontal">
-              <div className="field-label is-normal">
-                <label className="label">Name</label>
-              </div>
-              <div className="field-body">
-                <div className="field">
-                  <p className="control">
-                    <input id="name" className="input" type="text" value={state.name} />
-                    <input id="id" className="input" type="hidden" value={state.id} />
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="column">
-            <div className="field is-grouped">
-              <div className="control">
-                <button className="button" id="save">Save</button>
-              </div>
-              <div className="control">
-                <button className="button" id="showLoad">Load</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="columns">
-          <div className="column">
-            <textarea id="abcEditor" className="textarea code" rows="20">{state.document}</textarea>
-            <div id="warnings"></div>
-          </div>
-          <div className="column">
-            <div id="canvas"></div>
-          </div>
-        </div>
-      </div>
-
-      <div className="modal">
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Load Document</p>
-            <button className="delete" aria-label="close"></button>
-          </header>
-          <section className="modal-card-body">
-            Content
-          </section>
-          <footer className="modal-card-foot">
-            <button className="button is-success">Load</button>
-            <button className="button">Cancel</button>
-          </footer>
-        </div>
-      </div>
-
     </section>
   )
 }
@@ -95,6 +36,10 @@ function main(sources) {
     .map(([id, name, document, showDialog]) => {
       return { id, name, document, showDialog }
     }).startWith({id: 0, name: "", document: "", showDialog: false})
+
+  const abcEditor = ABCEditor({ DOM: sources.DOM, state: state$ }) //TODO append abcEditor.DOM in final view
+  const header = Header({ DOM: sources.DOM, state: state$ }) //TODO append header.DOM in final view
+  const loadDialog = LoadDialog({ DOM: sources.DOM, state: state$ }) //TODO append header.DOM in final view
 
   const saveRequest$ = saveClick$.compose((input) =>
     state$.take(1).map(state => {
