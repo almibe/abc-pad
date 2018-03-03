9e986a7280821187047967f68adad00cb244648f
Alex Berry <berry3@indiana.edu>
2018-03-03 17:39:14 -0500

    starting to add editor and header components + removing HelloWorld and cycle.js components, #18

---
+++ vue/src/components/ABCEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div className="container">
+    <div className="columns">
+      <div className="column">
+        <textarea id="abcEditor" className="textarea code" rows="20">{abcDoc}</textarea>
+        <div id="warnings"></div>
+      </div>
+      <div className="column">
+        <div id="canvas"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ABCEditor',
+  data () {
+    return {
+      abcDoc: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue/src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div className="container">
+    <div className="container">
+      <h1 className="title">ABC Editor</h1>
+    </div>
+
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label">Name</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <p className="control">
+                  <input id="name" className="input" type="text" value={docName} />
+                  <input id="id" className="input" type="hidden" value={docId} />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button" id="save">Save</button>
+            </div>
+            <div className="control">
+              <button className="button" id="showLoad">Load</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      docName: '',
+      docId: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
