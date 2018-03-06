31802d66aeb889a3ab5a8d258459c5bf664f4dd0
Alex Berry <berry3@indiana.edu>
2018-03-06 00:18:43 -0500

    adding imports to parcel build + starting to merge code, #20

--- parcel/src/App.vue
+++ parcel/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Header></Header>
+    <ABCEditor></ABCEditor>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header.vue'
+import ABCEditor from './components/ABCEditor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    ABCEditor
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- parcel/src/main.js
+++ parcel/src/main.js
@@ -1,12 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import router from './router' // TODO add back for #19
+import '../node_modules/bulma/css/bulma.css'
+import './main.css'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // router, //TODO add back for #19
   render: h => h(App)
 })
