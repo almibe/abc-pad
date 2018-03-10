bace03ebbc9e813726b0a94c234e7b6a066deb4d
Alex Berry <berry3@indiana.edu>
2018-03-09 19:21:07 -0500

    moving files to brunch project, #21

--- brunch/app/App.vue
+++ brunch/app/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Header></Header>
+    <ABCEditor></ABCEditor>
+    <LoadDialog></LoadDialog>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
-  }
+import Header from './components/Header.vue'
+import ABCEditor from './components/ABCEditor.vue'
+import LoadDialog from './components/LoadDialog.vue'
 
+export default {
+  name: 'App',
+  components: {
+    Header,
+    ABCEditor,
+    LoadDialog
+  }
+}
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
 </style>
--- brunch/app/main.js
+++ brunch/app/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import abcjs from "abcjs";
+// import router from './router' // TODO add back for #19
 import "vueify/lib/insert-css" // required for .vue file <style> tags
+import '../node_modules/bulma/css/bulma.css'
+import './main.css'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    render: h => h(App)
+  el: '#app',
+  // router, //TODO add back for #19
+  render: h => h(App)
 })
+
+window.onload = function () {
+  new abcjs.Editor('abcEditor', { canvas_id: 'canvas', generate_midi: false, warnings_id: 'warnings' })
+}
