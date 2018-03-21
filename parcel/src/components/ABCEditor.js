1e438bd3abdcdb0859da3e8edb49df34d65bb0d7
Alex Berry <berry3@indiana.edu>
2018-03-20 21:56:38 -0400

    fixing header spacing, adding T field to default abcdoc, #12

--- parcel/src/components/ABCEditor.js
+++ parcel/src/components/ABCEditor.js
@@ -5,7 +5,7 @@ export const ABCEditor = ({ }) => (
   <div class="container" oncreate={element => init()}>
     <div class="columns">
       <div class="column">
-        <textarea id="abcEditor" class="textarea code" rows="20"></textarea>
+        <textarea id="abcEditor" class="textarea code" rows="20">T: Unknown</textarea>
         <div id="warnings"></div>
       </div>
       <div class="column">
