e79a3a63376320f72857c6e49c4bc5b6c18fd462
Alex Berry <berry3@indiana.edu>
2018-05-28 14:36:15 -0400

    moving vuex code into store.js and out of main.js, #32

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,55 +1,10 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
-import axios from 'axios'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuex)
 
 new Vue({
   el: '#app',
   components: { App },
   render: h => h(App)
-})
-
-const store = new Vuex.Store({
-  state: {
-    documentId: -1,
-    showLoad: false,
-    document: 'T: Untitled\nC: Unknown\nK: ',
-    documentList: [],
-    status: ''
-  },
-  actions: {
-    saveDocument: function(state) {
-      if (state.documentId > 0) {
-        axios.patch('documents/'+state.documentId, {
-          document: state.document
-        })
-        .then(function (response) {
-          console.log(response); //TODO present feedback to user
-        })
-        .catch(function (error) {
-          console.log(error); //TODO present feedback to user
-        });
-      } else {
-        axios.post('documents/', {
-          document: state.document
-        })
-        .then(function (response) {
-          console.log(response); //TODO present feedback to user
-        })
-        .catch(function (error) {
-          console.log(error); //TODO present feedback to user
-        });
-      }
-    },
-    loadDocument: function(id) {
-      //TODO make axios call
-    }
-  },
-  mutations: {
-
-  }
 })
