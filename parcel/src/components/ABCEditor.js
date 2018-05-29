c2b7309847b36767edcfa4453620c8ea269456b8
Alex Berry <berry3@indiana.edu>
2018-05-28 23:57:51 -0400

    adding midi support to ABCEditor, closes #34

--- parcel/src/components/ABCEditor.js
+++ parcel/src/components/ABCEditor.js
@@ -1,5 +1,5 @@
 import { h } from 'hyperapp'
-import abcjs from 'abcjs'
+import abcjs from 'abcjs/midi'
 
 export const ABCEditor = ({ document, setText }) => (
   <div class="container" oncreate={element => init()}>
@@ -7,6 +7,7 @@ export const ABCEditor = ({ document, setText }) => (
       <div class="column">
         <textarea id="abcEditor" class="textarea code" rows="20" value={document} oninput={e => setText(e.target.value)}></textarea>
         <div id="warnings"></div>
+        <div id="midi"></div>
       </div>
       <div class="column">
         <div id="canvas"></div>
@@ -16,5 +17,11 @@ export const ABCEditor = ({ document, setText }) => (
 )
 
 function init() {
-  new abcjs.Editor('abcEditor', { canvas_id: 'canvas', generate_midi: false, warnings_id: 'warnings' })
+  new abcjs.Editor('abcEditor', {
+    canvas_id: 'canvas',
+    generate_midi: true,
+    midi_id: "midi",
+    generate_warnings: true,
+    warnings_id: 'warnings'
+  })
 }
