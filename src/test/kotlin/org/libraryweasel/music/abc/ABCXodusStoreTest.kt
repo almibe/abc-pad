ab578ca4616e01b6f8bcaab0101113f58b9085aa
Alex Berry <berry3@indiana.edu>
2018-06-01 21:03:38 -0400

    starting to add tests, #36

---
+++ src/test/kotlin/org/libraryweasel/music/abc/ABCXodusStoreTest.kt
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.libraryweasel.music.abc
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class ABCXodusStoreTest : StringSpec({
+    "start with zero documents" {
+        "test".length shouldBe 4
+    }
+
+    "test adding two documents" {
+        "test".length shouldBe 4
+    }
+
+    "test details for two documents and fetch" {
+        "test".length shouldBe 4
+    }
+
+    "edit documents" {
+        "test".length shouldBe 4
+    }
+
+    "test new details and fetch" {
+        "test".length shouldBe 4
+    }
+
+    "remove one document" {
+        "test".length shouldBe 4
+    }
+
+    "test details of remaining document and fetch" {
+        "test".length shouldBe 4
+    }
+
+    "add document for another user" {
+        "test".length shouldBe 4
+    }
+
+    "make sure that accounts are being used with details and fetching" {
+        "test".length shouldBe 4
+    }
+})
