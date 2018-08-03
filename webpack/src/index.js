9217a8702535a41b037892e19ed2b862b689f3fa
Alex Berry <berry3@indiana.edu>
2018-08-02 23:11:51 -0400

    starting to fill in base html and js

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,13 @@
-console.log("I'm a silly entry point");
+import abcjs from 'abcjs/midi'
+import './main.css'
+import 'abcjs/abcjs-midi.css'
+require('./main.scss')
+//import 'font-awesome/css/font-awesome.css'
+
+new abcjs.Editor('abcEditor', {
+  canvas_id: 'canvas',
+  generate_midi: true,
+  midi_id: "midi",
+  generate_warnings: true,
+  warnings_id: 'warnings'
+})
