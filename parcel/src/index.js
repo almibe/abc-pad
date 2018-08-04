e65b83e618d4ab7442d58f336799af668fc5a770
Alex Berry <berry3@indiana.edu>
2018-08-03 21:50:51 -0400

    getting rid of webpack dir

--- parcel/src/index.js
+++ parcel/src/index.js
@@ -36,3 +36,16 @@ bus.on('save request', () => {
 //bus.on('', function() {
 //
 //})
+import abcjs from 'abcjs/midi'
+import './main.css'
+import 'abcjs/abcjs-midi.css'
+require('./main.scss')
+//import 'font-awesome/css/font-awesome.css'
+
+new abcjs.Editor('abcEditor', {
+  canvas_id: 'canvas',
+  generate_midi: true,
+  midi_id: "midi",
+  generate_warnings: true,
+  warnings_id: 'warnings'
+})
