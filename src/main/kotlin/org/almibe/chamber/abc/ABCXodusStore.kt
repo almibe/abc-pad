19a90b3950e11c9e968a7895a742c91c60b2c53d
Alex Michael Berry <berry3@indiana.edu>
2018-11-11 22:56:07 -0500

    starting chamber rewrite, #48

--- src/main/kotlin/org/almibe/chamber/abc/ABCXodusStore.kt
+++ src/main/kotlin/org/almibe/chamber/abc/ABCXodusStore.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.libraryweasel.music.abc
+package org.almibe.chamber.abc
 
 import io.vertx.ext.auth.User
 import jetbrains.exodus.entitystore.Entity
